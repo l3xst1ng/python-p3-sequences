29c5542b9a6c9738a415e2f6aed4c85cf5b0148f
l3xst1ng <howdy.martt@gmail.com>
2024-05-26 20:23:03 +0300

    Sequence lab hotfix

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
+
 def print_fibonacci(length):
+    # Initialization
     num1 = 0
     num2 = 1
 
-    # Empty list to store the Fibonacci sequence
+    # Empty list to store the sequence
     fibonacci_sequence = []
 
-    # Handle the case of length = 0
+
     if length == 0:
         print(fibonacci_sequence)
         return
 
-    # Checking for valid length
+    # Checking for valid lengths
     if length < 0:
         print("Invalid length. Length should be a non-negative integer.")
         return
 
-    # Adding the first two numbers to the equence list
+    # Adding the first two numbers to the sequence list
     fibonacci_sequence.append(num1)
     if length >= 2:
         fibonacci_sequence.append(num2)
@@ -33,5 +35,5 @@ def print_fibonacci(length):
         num1 = num2
         num2 = next_num
 
-
+    
     print(fibonacci_sequence)
